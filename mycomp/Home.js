91f8526548dc7c12701e9ce21039f2fcab5fe0bf
DheenodaraRao <dheenodararao@gmail.com>
2018-07-25 22:16:05 +0800

    Implemented Dropdown CLoud Feature

--- mycomp/Home.js
+++ mycomp/Home.js
@@ -11,12 +11,14 @@ import {
   Text, ScrollView,
   View,
   Picker,
+  Alert,
 } from 'react-native';
 import { FloatingAction } from 'react-native-floating-action';
 import MyCard from './MyCard';
 import PickerWithLabel from './PickerWithLabel';
 
 let mystates = require('./StateCodes');
+let path = require('../config/serverpath');
 
 const actions = [{
   text: 'History',
@@ -32,8 +34,55 @@ export default class Home extends Component {
         super(props)
         this.state = {
           'selectedState' : '',
+          'isFetching' : false,
+          'noStateSelected': true,
+          'places' : [],
+
         }
-    };
+
+        this._load = this._load.bind(this);
+    }
+
+    componentDidMount(){
+
+      this._load();
+    }
+
+    _load(){
+      let serverpath = path.serverpath;
+      let fullpath = '';
+      if(this.state.noStateSelected){
+        fullpath = serverpath + '/api/places';
+      }
+      else{
+        fullpath = serverpath + '/api/places/'+ this.state.selectedState; 
+      }
+
+      this.setState({isFetching: true});
+      console.log(fullpath);
+      fetch(fullpath, {method: 'GET',                        // Specifies an HTTP POST request
+      headers: {
+        Accept: 'application/json',          // The client sends data in JSON
+        'Content-Type': 'application/json',  // The response content is in JSON
+      }})
+      .then((response) => {
+        if(!response.ok){
+          Alert.alert('Error',response.status.toString());
+          throw Error('Error'+ response.status);
+        }
+        console.log('******', response);
+        return response.json()
+      })
+      .then((places) => {
+        console.log('######', places);
+        this.setState({places: places});
+        //console.log('CurrentState: '+ this.state.places);
+        this.setState({isFetching: false});
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+    }
 
   render() {
     return (
@@ -49,13 +98,32 @@ export default class Home extends Component {
         items={mystates.states}
         value={this.state.selectedState}
         onValueChange={(itemValue, itemIndex) => {
-          this.setState({selectedState: itemValue})
+          this.setState({
+            selectedState: itemValue,
+            noStateSelected: false,
+          }, () => {
+            this._load();
+          })
+          console.log('++++++Pressed', itemValue);
+          console.log('State: ', this.state.selectedState);
         }}
       />
 
       {/*Testing out picker*/}
-      <Text> State is {this.state.selectedState}</Text>
+      {/*<Text> State is {this.state.selectedState}</Text>
+      <Text>{this.state.places.length}</Text>
+      {console.log('+++++++place state: ',this.state.places)}
+      <Text>{path.serverpath +' '+ this.state.isFetching }</Text>
+      */}
 
+      {this.state.places.map(item => 
+      <MyCard 
+      cardTitle={item.placename}
+      cardImageLink={item.imageurl}
+      cardContent={item.location}
+      
+      />)
+      }
     </ScrollView>
 
         {/*Floating Action Component*/}
